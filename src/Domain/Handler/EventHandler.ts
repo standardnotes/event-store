5b4cd431c5f0503ffc9ec805e7e5a6d1cba9d643
Karol Sójko <karolsojko@protonmail.com>
2021-12-20 11:18:05 +0100

    fix: add debug logs for handling domain events

--- src/Domain/Handler/EventHandler.ts
+++ src/Domain/Handler/EventHandler.ts
@@ -2,6 +2,7 @@ import { DomainEventHandlerInterface, DomainEventInterface } from '@standardnote
 import { TimerInterface } from '@standardnotes/time'
 import { inject, injectable } from 'inversify'
 import { Connection } from 'typeorm'
+import { Logger } from 'winston'
 import TYPES from '../../Bootstrap/Types'
 import { Event } from '../Event/Event'
 
@@ -10,10 +11,13 @@ export class EventHandler implements DomainEventHandlerInterface {
   constructor(
     @inject(TYPES.Timer) private timer: TimerInterface,
     @inject(TYPES.DBConnection) private db: Connection,
+    @inject(TYPES.Logger) private logger: Logger
   ) {
   }
 
   async handle(event: DomainEventInterface): Promise<void> {
+    this.logger.debug('Handling event: %O', event)
+
     const storedEvent = new Event()
     storedEvent.eventType = event.type
     storedEvent.userIdentifier = event.meta.correlation.userIdentifier
